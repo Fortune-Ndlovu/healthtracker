b12eb4a0bad410ce56f96bc77d79c5050fe24444
Fortune-Ndlovu <ndlovufortune97@gmail.com>
2025-09-19 22:21:22 +0100

    Import the models.User data class within the models package and assign it to a user variable and call the class in main(). It shows no new properties yet during instantiation because we have not assigin any CLI args as params to the key in the class a property. we need a way for user input within tthe cli before instantiation of the user data class.
    Signed-off-by: Fortune-Ndlovu <ndlovufortune97@gmail.com>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
+import models.User
+
+var user = User()
+
 fun main() {
     println("Welcome to Health Tracker")
+    println("Please enter the following for the user:")
+    println("   Name: ")
+    println("   Email: ")
+    println("   Id: ")
+    println("The user details are: " + user)
 }
